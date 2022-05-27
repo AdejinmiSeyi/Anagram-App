bc1c2ad10b44fe849cd3353f64fef379170b7d1e
AdejinmiSeyi <seyifeyisara@gmail.com>
2022-05-27 13:41:23 +0100

    Anagram App
    This app compares to string and indicates whether they are anagrams

---
+++ main.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+word = input('Enter first word: ')
+anagram = input('Enter second word: ')
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    
+    if (sorted(word) == sorted(anagram)):
+        return True
+
+    else:
+        return False
+
+print(find_anagram(word, anagram))
+
